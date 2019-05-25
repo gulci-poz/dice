a81370bbbc91dc0e7caa4f275d242b2300bc565b
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2019-05-25 06:55:24 +0200

    animating RollDice

--- src/Die.js
+++ src/Die.js
@@ -3,7 +3,7 @@ import './Die.css';
 
 class Die extends Component {
     render() {
-        return <i className={`Die fas fa-dice-${this.props.face}`}></i>;
+        return <i className={`Die ${this.props.rolling && "shaking"} fas fa-dice-${this.props.face}`}></i>;
     }
 }
 
--- src/RollDice.js
+++ src/RollDice.js
@@ -9,14 +9,20 @@ class RollDice extends Component {
 
     state = {
         die1: 'one',
-        die2: 'one'
+        die2: 'one',
+        isRolling: false
     };
 
     roll = () => {
         this.setState({
             die1: this.getRandomFace(),
-            die2: this.getRandomFace()
-        })
+            die2: this.getRandomFace(),
+            isRolling: true
+        });
+
+        setTimeout(() => {
+            this.setState({isRolling: false});
+        }, 1000);
     };
 
     getRandomFace() {
@@ -27,10 +33,12 @@ class RollDice extends Component {
         return (
             <div className="RollDice">
                 <div className="RollDice-container">
-                    <Die face={this.state.die1}/>
-                    <Die face={this.state.die2}/>
+                    <Die face={this.state.die1} rolling={this.state.isRolling}/>
+                    <Die face={this.state.die2} rolling={this.state.isRolling}/>
                 </div>
-                <button onClick={this.roll}>Roll Dice!</button>
+                <button onClick={this.roll} disabled={this.state.isRolling}>
+                    {this.state.isRolling ? 'Rolling...' : 'Roll Dice!'}
+                </button>
             </div>
         );
     }
