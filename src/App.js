0c177776700844885bebb3856ce9066e5d33d842
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2019-05-25 05:29:42 +0200

    Die component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Die from "./Die";
 
 function App() {
     return (
         <div className="App">
+            <Die face="five"/>
+            <Die face="two"/>
         </div>
     );
 }
